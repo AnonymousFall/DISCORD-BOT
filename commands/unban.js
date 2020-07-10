a4f1053221079d625b60eabb495e9def13e5f885
AnonymousFall <68094401+AnonymousFall@users.noreply.github.com>
2020-07-11 11:11:33 +1200

    TAKE 2 unban

--- commands/unban.js
+++ commands/unban.js
@@ -3,9 +3,28 @@ module.exports = {
     description: "this is the ban command",
     execute(message, args){
  
+        if(!message.member.haspermission(["BAN_MEMBERS", "ADMINISTRATOR"])) return message.channel.send("You don't have permission to perform this command!")
 
-                args[0].send('MESSAGE / INVITE LINK');
-                message.guild.unban(args[0]);
+        let bannedMember = await bot.fetchUser(args[0])
+        if(!bannedMember) return message.channel.send("Please provide a user id to unban someone!")
+
+        let reason = args.slice(1).join(" ")
+        if(!reason)reason = "No reason given!"
+
+        if(!message.guild.me.haspermission(["BAN_MEMBERS","ADMINISTRATOR"])) return message.channel.send("I dont have permission to perform this command")
+        // message.delete()
+        try {
+
+            message.guild.unban(bannedMember,{reason: reason})
+            message.channel.send(`${bannedMember.tag} has been unbanned from the server!`)
+    
+        } catch(e) {
+            console.log(e.message)
+            
+        }
+        
+        // bannedMember.send('MESSAGE / INVITE LINK');
+        //         message.guild.unban(bannedMember);
 
 
         }
