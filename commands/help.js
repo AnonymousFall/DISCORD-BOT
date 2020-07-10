ab70648734d9863faaeed4021f5307e03f94b5aa
AnonymousFall <68094401+AnonymousFall@users.noreply.github.com>
2020-07-10 20:24:53 +1200

    customised the MessageEmbed

--- commands/help.js
+++ commands/help.js
@@ -6,21 +6,20 @@ module.exports = {
 
         const exampleEmbed = new Discord.MessageEmbed()
             .setColor('#ff0000')
-            .setTitle('Some title')
-            .setURL('https://discord.js.org/')
-            .setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
-            .setDescription('Some description here')
-            .setThumbnail('https://i.imgur.com/wSTFkRM.png')
+            .setTitle('Help')
+            .setAuthor('Fall', 'https://i.imgur.com/wSTFkRM.png')
+            .setDescription('Help commands for falls Discord bot')
+            // .setThumbnail('https://i.imgur.com/wSTFkRM.png')
             .addFields(
                 { name: 'Regular field title', value: 'Some value here' },
                 { name: '\u200B', value: '\u200B' },
                 { name: 'Inline field title', value: 'Some value here', inline: true },
                 { name: 'Inline field title', value: 'Some value here', inline: true },
             )
-            .addField('Inline field title', 'Some value here', true)
-            .setImage('https://i.imgur.com/wSTFkRM.png')
+            // .addField('Inline field title', 'Some value here', true)
+            // .setImage('https://i.imgur.com/wSTFkRM.png')
             .setTimestamp()
-            .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+            .setFooter('Happy to help', 'https://i.imgur.com/wSTFkRM.png');
     
     message.channel.send(exampleEmbed);    }
 }
