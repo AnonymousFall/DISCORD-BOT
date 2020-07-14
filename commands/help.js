2e6cab163783d32f5648b7c144cfb8b2c96de502
AnonymousFall <68094401+AnonymousFall@users.noreply.github.com>
2020-07-14 21:44:04 +1200

    Take 2

--- commands/help.js
+++ commands/help.js
@@ -5,15 +5,15 @@ module.exports = {
         const Discord = require('discord.js');
 
         const exampleEmbed = new Discord.MessageEmbed()
-            .setColor('#4d004d')
+            .setColor('#78AB46')
             .setTitle('Help')
-            .setAuthor('Fall', 'https://pbs.twimg.com/profile_images/1275574904742506496/GnR_f3aA_400x400.jpg')
+            .setAuthor('Fall', 'https://ichef.bbci.co.uk/news/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg')
             .setDescription('Help commands for falls Discord bot')
-            .setThumbnail('https://pbs.twimg.com/profile_images/1275574904742506496/GnR_f3aA_400x400.jpg')
+            .setThumbnail('https://ichef.bbci.co.uk/news/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg')
             // .addField('Inline field title', 'Some value here', true)
             // .setImage('https://i.imgur.com/wSTFkRM.png')
             .setTimestamp()
-            .setFooter('Happy to help', 'https://pbs.twimg.com/profile_images/1275574904742506496/GnR_f3aA_400x400.jpg');
+            .setFooter('Happy to help', 'https://ichef.bbci.co.uk/news/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg');
 
         args[0].forEach(element => {
             exampleEmbed.addField( args[1]+element.name, element.description, true)
